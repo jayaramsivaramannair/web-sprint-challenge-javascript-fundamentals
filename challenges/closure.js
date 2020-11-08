9d8a1bb4dc9eac3c3c11856dc49b88581e3195d7
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2020-11-08 08:23:10 -0500

    Completed Task 2 in closure.js

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,6 +17,7 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
+
 `Nested function can access the variable internal because of closure. Closure enables a nested function to not only access variable within its own function scope but also variables defined in the global scope
 as well as variables defined in the scope of its surrounding function. Surrounding function in this case is myFunction()`
 
@@ -24,3 +25,17 @@ as well as variables defined in the scope of its surrounding function. Surroundi
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(num) {
+  let counter = 0;
+  return function retCount() {
+    for(let i = num; num >= 1; num-- ) {
+      counter += num;
+    }
+    return counter;
+  }
+}
+
+
+let logSum = summation(2);
+console.log(logSum());
